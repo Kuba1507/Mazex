8b5abc68cabc5a3b661e6e841329c1ee106b1ca9
Kuba1507 <kuba.romanski@onet.com.pl>
2023-11-21 08:03:22 +0100

    scripted yearsAmountControl

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,45 @@
-const body = document.querySelector('body')
-const burgerBtn = document.querySelector('.hamburger')
-const mobileNav = document.querySelector('.mobile-nav')
-const mobileNavItems = document.querySelectorAll('.mobile-nav__item')
+const body = document.querySelector("body");
+const burgerBtn = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileNavItems = document.querySelectorAll(".mobile-nav__item");
+const yearsAmount = document.querySelector(".years-number");
 
 const handleNav = () => {
-	mobileNav.classList.toggle('show-nav')
-	burgerBtn.classList.toggle('is-active')
-	body.classList.toggle('disable-scroll')
-}
+	mobileNav.classList.toggle("show-nav");
+	burgerBtn.classList.toggle("is-active");
+	body.classList.toggle("disable-scroll");
+};
 
 const closeNav = () => {
-	mobileNav.classList.remove('show-nav')
-	burgerBtn.classList.remove('is-active')
-}
+	mobileNav.classList.remove("show-nav");
+	burgerBtn.classList.remove("is-active");
+};
 
-burgerBtn.addEventListener('click', handleNav)
-mobileNavItems.forEach(item => {
-	item.addEventListener('click', closeNav)
-})
+const yearsAmountControl = () => {
+	const today = new Date();
+	const mm = today.getMonth() + 1;
+	const yyyy = today.getFullYear();
+
+	const baseYear = 2025; // Rok, od którego zaczynają się zmiany liczby lat
+	const startingValue = 2; // Początkowa wartość liczby lat
+
+	// Obliczamy różnicę między aktualnym rokiem a bazowym rokiem
+	const yearDifference = yyyy - baseYear;
+
+	// Jeśli obecna data jest po kwietniu, zwiększamy różnicę o 1
+	const adjustedDifference = mm > 3 ? yearDifference + 1 : yearDifference;
+
+	// Wyliczamy nową ilość lat
+	const newYears = Math.max(startingValue + adjustedDifference, startingValue);
+
+	// Ustawiamy wartość w akapicie HTML
+	yearsAmount.textContent = newYears.toString();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	yearsAmountControl();
+});
+burgerBtn.addEventListener("click", handleNav);
+mobileNavItems.forEach((item) => {
+	item.addEventListener("click", closeNav);
+});
