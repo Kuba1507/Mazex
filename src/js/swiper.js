8e104a6a9de0a1b813ba63e34bdbc6c884045425
Kuba1507 <kuba.romanski@onet.com.pl>
2023-11-22 08:33:49 +0100

    working with swiper js

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,11 +1,11 @@
-const swiper = new Swiper(".swiper", {
-	slidesPerView: "auto",
-	loop: true,
-	centeredSlides: true,
-	speed: 8000,
-	allowTouchMove: false,
-	disableOnInteraction: false,
-	autoplay: {
-		delay: 1,
-	},
-});
+// const swiper = new Swiper(".swiper", {
+// 	slidesPerView: "auto",
+// 	loop: true,
+// 	centeredSlides: true,
+// 	speed: 8000,
+// 	allowTouchMove: false,
+// 	disableOnInteraction: false,
+// 	autoplay: {
+// 		delay: 1,
+// 	},
+// });
