bd0ab0dc5cb9e7588bd9289fe299e046309f4d89
Kuba1507 <kuba.romanski@onet.com.pl>
2023-11-22 07:38:00 +0100

    styled partners section for desktops and cleared js code

---
+++ src/js/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: "auto",
+    loop: true,
+    centeredSlides: true,
+    speed: 8000,
+    allowTouchMove: false,
+    disableOnInteraction: false,
+    autoplay: {
+        delay: 1,
+    }
+});
