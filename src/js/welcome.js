47127f7bab1eaa6bf6dad9fbe78f4491976d1af1
Kuba1507 <kuba.romanski@onet.com.pl>
2024-05-28 10:38:16 +0200

    scripted index to move to garage

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -1,7 +1,13 @@
 const transportBtn = document.querySelector('.welcome-section__transport-cta-button')
+const garageBtn = document.querySelector('.welcome-section__garage-cta-button')
 
-const moveToTransport = params => {
+const moveToTransport = () => {
 	window.location.href = 'transport.html'
 }
 
+const moveToGarage = () => {
+	window.location.href = 'serwis.html'
+}
+
 transportBtn.addEventListener('click', moveToTransport)
+garageBtn.addEventListener('click', moveToGarage)
